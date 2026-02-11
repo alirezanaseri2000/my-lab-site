cd63c3d9d1620e1494eed80047eae41e941717fe
Alireza Naseri <alireza.naseri79@yahoo.com>
2026-02-11 00:28:15 -0500

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".menu-btn");
-const nav = document.querySelector("#nav");
-
-if (btn && nav) {
-  btn.addEventListener("click", () => {
-    const isOpen = nav.classList.toggle("open");
-    btn.setAttribute("aria-expanded", isOpen ? "true" : "false");
-  });
-}
